3b0202152b5e57c02912b8bbfe07e1d3f528bda7
s-cryptor <grisga2001@gmail.com>
2022-01-06 21:11:52 +0300

    add error logger

--- src/api-cmc/api-cmc.service.ts
+++ src/api-cmc/api-cmc.service.ts
@@ -12,37 +12,45 @@ interface IQuery {
 @Injectable()
 export class ApiCmcService {
   async getMap(query: IQuery): Promise<object> {
-    const res = await axios.get(
-      `${process.env.CMC_URL}/v1/cryptocurrency/map`,
-      {
-        headers: {
-          'X-CMC_PRO_API_KEY': process.env.CMC_TOKEN
-        },
-        params: {
-          start: +query.start,
-          limit: +query.limit,
-          sort: query.sort
+    try {
+      const res = await axios.get(
+        `${process.env.CMC_URL}/v1/cryptocurrency/map`,
+        {
+          headers: {
+            'X-CMC_PRO_API_KEY': process.env.CMC_TOKEN
+          },
+          params: {
+            start: +query.start,
+            limit: +query.limit,
+            sort: query.sort
+          }
         }
-      }
-    )
+      )
 
-    return res.data
+      return res.data
+    } catch (error) {
+      console.error(error.response.data)
+    }
   }
 
   async getListingsLatest(query: IQuery): Promise<object> {
-    const res = await axios.get(
-      `${process.env.CMC_URL}/v1/cryptocurrency/listings/latest`,
-      {
-        headers: {
-          'X-CMC_PRO_API_KEY': process.env.CMC_TOKEN
-        },
-        params: {
-          start: +query.start,
-          limit: +query.limit
+    try {
+      const res = await axios.get(
+        `${process.env.CMC_URL}/v1/cryptocurrency/listings/latest`,
+        {
+          headers: {
+            'X-CMC_PRO_API_KEY': process.env.CMC_TOKEN
+          },
+          params: {
+            start: +query.start,
+            limit: +query.limit
+          }
         }
-      }
-    )
+      )
 
-    return res.data
+      return res.data
+    } catch (error) {
+      console.error(error.response.data)
+    }
   }
 }
